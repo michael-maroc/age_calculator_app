c6172157ecb1f5f8bc54e1f02086d20a1b000888
Michael <maroc.michael@hotmail.com>
2023-04-21 19:24:37 +0200

    mise à jour de la logique de calcul d'âge et remise à 0 des valeurs du formulaire après validation

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,6 @@
+// Form
+const form = document.getElementById("form");
+
 // Labels
 const dayLabel = document.getElementById('dayLabel');
 const monthLabel = document.getElementById('monthLabel');
@@ -8,34 +11,49 @@ const dayInput = document.getElementById('dayInput');
 const monthInput = document.getElementById('monthInput');
 const yearInput = document.getElementById('yearInput');
 
+// Calculated values
+let yearsValue = document.getElementById('yearsValue');
+let monthsValue = document.getElementById('monthsValue');
+let daysValue = document.getElementById('daysValue');
+
+// Error messages
+const allErrMsg = document.querySelectorAll('.error-msg');
+const dayErrMsg = document.querySelector('.dayErrMsg');
+const monthErrMsg = document.querySelector('.monthErrMsg');
+const yearErrMsg = document.querySelector('.yearErrMsg');
+
 // Button
 const btn = document.querySelector('.btn');
 
-// Calculated values
-const yearsValue = document.getElementById('yearsValue');
-const monthsValue = document.getElementById('monthsValue');
-const daysValue = document.getElementById('daysValue');
-
 // Dates
 const today = new Date();
-const dayNow = today.getUTCDate();
-const monthNow = today.getUTCMonth() +1;
-const yearNow = today.getFullYear();
+let dayNow = today.getUTCDate();
+let monthNow = today.getUTCMonth() +1;
+let yearNow = today.getFullYear();
 
-// Array of days numbers in each month
-const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+// Array of numbers of days for each month
+const monthsArr = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-btn.addEventListener('click', function(){
-  let year = yearNow - yearInput.value;
-  let month = monthNow - monthInput.value;
-  let day = months[monthInput.value -1] - dayInput.value
+btn.addEventListener('click', function(e){
+  e.preventDefault();
 
-  if(month < 0) {
-    month = month + 12;
-    year--;
+  if (dayInput.value > dayNow) {
+    dayNow = dayNow + monthsArr[monthNow -1];
+    monthNow--;
   }
 
-  daysValue.textContent = day;
-  monthsValue.textContent = month;
-  yearsValue.textContent = year;
+  if (monthInput.value > monthNow) {
+    monthNow = monthNow + 12;
+    yearNow--;
+  }
+
+  let d = dayNow - dayInput.value;
+  let m = monthNow - monthInput.value;
+  let y = yearNow - yearInput.value;
+
+  daysValue.textContent = d;
+  monthsValue.textContent = m;
+  yearsValue.textContent = y;
+
+  form.reset();
 });
